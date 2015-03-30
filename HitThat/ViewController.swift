1405293021146ee9ed53ea06d8d4aa86ecefb9ab
Jake Seaton <jakeseaton13@gmail.com>
2015-03-30 11:08:44 -0400

    initial commit. push working

---
+++ HitThat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HitThat
+//
+//  Created by Jake Seaton on 3/30/15.
+//  Copyright (c) 2015 Jake Seaton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
